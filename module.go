3deb8e3577a90d945aaefc8f7555f4c953a0180e
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-18 17:44:20 +0100

    Group package-level variables

--- module.go
+++ module.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-05-09 17:22:37 076AE8                             zr-win/[module.go]
+// :v: 2019-05-18 17:44:10 F9A2DD                             zr-win/[module.go]
 // -----------------------------------------------------------------------------
 
 // Package win provides Windows API function and
@@ -13,10 +13,12 @@ import (
 	"github.com/balacode/zr"
 )
 
-// PL is fmt.Println() but is used only for debugging.
-var PL = fmt.Println
+var (
+	// PL is fmt.Println() but is used only for debugging.
+	PL = fmt.Println
 
-// VL is zr.VerboseLog() but is used only for debugging.
-var VL = zr.VerboseLog
+	// VL is zr.VerboseLog() but is used only for debugging.
+	VL = zr.VerboseLog
+)
 
 //end
